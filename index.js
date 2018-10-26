7b71525966d10eea363b8e4b0b32aa103c5d9edd
Mark Dewey <mildewey@gmail.com>
2018-10-26 15:20:53 -0600

    Allowing a global prefix to be added for all channels.

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-channel = require('./channel.js')
+const channel = require('./channel.js')
+let prefix = ''
 
 exports.subscribe = function (name, handler) {
-  return channel.registerHandler(name, handler)
+  let prefixedName = prefix.length !== 0 ? `${prefix}/${name}` : name
+  return channel.registerHandler(prefixedName, handler)
 }
 
 exports.publish = function (name, data) {
-  channels = channel.getChannels(channel.parseChannelNames(name))
+  let prefixedName = prefix.length !== 0 ? `${prefix}/${name}` : name
+  channels = channel.getChannels(channel.parseChannelNames(prefixedName))
   return Promise.all(channels.map(chan => {
     chan.broadcast.postMessage({channel: chan.name, data})
-    return callHandlers(name, chan.handlers, data)
+    return callHandlers(prefixedName, chan.handlers, data)
   }))
+}
+
+exports.prefix = function (pre) {
+  prefix = pre
 }
